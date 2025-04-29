994cadf51fe75f13a391d98fccc6151ba9232bab
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-04-29 13:04:50 +0700

    mvp soal 1 done

--- soal_1/image_client.c
+++ soal_1/image_client.c
@@ -11,6 +11,7 @@
 int connect_socket();
 void handler(int sockfd, int opt);
 int send_file(int sockfd, const char *filename);
+int recvto_file(int sock, char *filename, size_t size);
 
 int main(int argc, char *argv[]) {
     int sockfd = connect_socket();
@@ -54,6 +55,7 @@ int connect_socket() {
 void handler(int sockfd, int opt) {
     char *folder = "secrets";
     char filename[256];
+
     char target[512];
 
     switch (opt) {
@@ -61,16 +63,26 @@ void handler(int sockfd, int opt) {
             printf("Enter the file name: ");
             scanf("%s", filename);
             snprintf(target, sizeof(target), "%s/%s", folder, filename);
-            if (send_file(sockfd, target) < 0) {
-                perror("send failed");
-            };
+            send(sockfd, "decode\n", strlen("decode\n"), 0);
+            send_file(sockfd, target);
             break;
         case 2:
             printf("Enter the file name to download: ");
             scanf("%s", filename);
+            send(sockfd, "download\n", strlen("download\n"), 0);
+            snprintf(target, sizeof(target), "%s\n", filename);
+            sleep(0.1);
+            send(sockfd, target, strlen(target), 0);
+            char size_buffer[32];
+            ssize_t size_len = recv(sockfd, size_buffer, sizeof(size_buffer) - 1, 0);
+            if (size_len <= 0) break;
+            size_buffer[size_len] = '\0';
+            size_t expected_size = strtoul(size_buffer, NULL, 10);
+            recvto_file(sockfd, filename, expected_size);
             break;
         case 3:
             close(sockfd);
+            exit(EXIT_SUCCESS);
             return;
         default:
             printf("Invalid option\n");
@@ -78,20 +90,67 @@ void handler(int sockfd, int opt) {
 }
 
 int send_file(int sockfd, const char *filename) {
-    char *action = "decode\n";
-    if (send(sockfd, action, strlen(action), 0) < 0) return -1;
-
-    FILE *fp;
-    char buffer[1024];
-
-    fp = fopen(filename, "rb");
+    FILE *fp = fopen(filename, "rb");
     if (!fp) return -1;
 
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        if (send(sockfd, buffer, sizeof(buffer), 0) < 0) {
-            break;
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    rewind(fp);
+
+    char size_str[32];
+    snprintf(size_str, sizeof(size_str), "%ld\n", file_size);
+
+    if (send(sockfd, size_str, strlen(size_str), 0) < 0) {
+        fclose(fp);
+        return -1;
+    }
+    sleep(0.1);
+
+    char *buffer = malloc(file_size);
+    if (!buffer) {
+        fclose(fp);
+        return -1;
+    }
+
+    size_t read_size = fread(buffer, 1, file_size, fp);
+    fclose(fp);
+
+    if (read_size != file_size) {
+        free(buffer);
+        return -1;
+    }
+
+    if (send(sockfd, buffer, file_size, 0) < 0) {
+        free(buffer);
+        return -1;
+    }
+
+    free(buffer);
+    return 0;
+}
+
+int recvto_file(int sock, char *filename, size_t size) {
+    ssize_t buflen;
+    FILE *fp;
+    char buffer[1024];
+    size_t total_received = 0;
+
+    fp = fopen(filename, "wb");
+    if (!fp) return -1;
+
+    while (total_received < size) {
+        size_t remaining = size - total_received;
+        size_t to_recv = sizeof(buffer);
+        if (remaining < to_recv) to_recv = remaining;
+
+        buflen = recv(sock, buffer, to_recv, 0);
+        if (buflen <= 0) {
+            fclose(fp);
+            return -1;
         }
-        bzero(buffer, sizeof(buffer));
+
+        fwrite(buffer, 1, buflen, fp);
+        total_received += buflen;
     }
 
     fclose(fp);
--- soal_1/image_server.c
+++ soal_1/image_server.c
@@ -1,10 +1,14 @@
+#define _XOPEN_SOURCE 700
+
 #include <arpa/inet.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -13,27 +17,53 @@
 void daemonize();
 int make_socket();
 void handler(int sock);
-int recvto_file(int sock, char *filename);
+int recvto_file(int sock, char *filename, size_t size);
 char *read_file(char *filename, long *file_size);
 int write_file(char *filename, char *content, size_t size);
+int send_file(int sockfd, const char *filename);
 void revstr(char *str);
-size_t hexs2bin(const char *hex, unsigned char **out);
-
+size_t hexs2bin(char *hex, unsigned char **out);
 int decoder_file(int sock);
 int downloader_file(int sock);
+void reap_zombies(int sig);
 
 int main(int argc, char *argv[]) {
-    // daemonize();
-    int sock = make_socket();
-    if (sock < 0) {
-        perror("socket creation failed");
+    daemonize();
+
+    struct sigaction sa;
+    sa.sa_handler = reap_zombies;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGCHLD, &sa, NULL);
+
+    int sockfd = make_socket();
+    if (sockfd < 0) {
+        perror("socket setup failed");
         exit(EXIT_FAILURE);
     }
 
-    handler(sock);
+    struct sockaddr_in client_addr;
+    socklen_t addr_len = sizeof(client_addr);
 
-    close(sock);
+    while (1) {
+        int client_sock = accept(sockfd, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_sock < 0) continue;
 
+        pid_t pid = fork();
+        if (pid < 0) {
+            close(client_sock);
+            continue;
+        } else if (pid == 0) {
+            close(sockfd);
+            handler(client_sock);
+            close(client_sock);
+            exit(0);
+        } else {
+            close(client_sock);
+        }
+    }
+
+    close(sockfd);
     return 0;
 }
 
@@ -49,9 +79,13 @@ void daemonize() {
     for (int x = sysconf(_SC_OPEN_MAX); x > 0; x--) close(x);
 }
 
+void reap_zombies(int sig) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
 int make_socket() {
     struct sockaddr_in address;
-    int sockfd, sock;
+    int sockfd;
     socklen_t addrlen = sizeof(address);
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) return -1;
@@ -60,11 +94,13 @@ int make_socket() {
     address.sin_addr.s_addr = htonl(INADDR_ANY);
     address.sin_port = htons(PORT);
 
-    if ((bind(sockfd, (struct sockaddr *)&address, addrlen)) < 0) return -1;
-    if (listen(sockfd, 3) < 0) return -1;
-    if ((sock = accept(sockfd, (struct sockaddr *)&address, &addrlen)) < 0) return -1;
+    int opt = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    return sock;
+    if ((bind(sockfd, (struct sockaddr *)&address, addrlen)) < 0) return -1;
+    if (listen(sockfd, 5) < 0) return -1;
+
+    return sockfd;
 }
 
 void handler(int sock) {
@@ -74,7 +110,6 @@ void handler(int sock) {
         if (buflen <= 0) break;
         action[buflen] = '\0';
         if (action[buflen - 1] == '\n') action[buflen - 1] = '\0';
-
         if (strcmp(action, "exit") == 0)
             break;
         else if (strcmp(action, "decode") == 0)
@@ -84,25 +119,31 @@ void handler(int sock) {
     }
 }
 
-int recvto_file(int sock, char *filename) {
+int recvto_file(int sock, char *filename, size_t size) {
     ssize_t buflen;
     FILE *fp;
     char buffer[1024];
+    size_t total_received = 0;
 
     fp = fopen(filename, "wb");
     if (!fp) return -1;
 
-    while (1) {
-        buflen = recv(sock, buffer, sizeof(buffer), 0);
+    while (total_received < size) {
+        size_t remaining = size - total_received;
+        size_t to_recv = sizeof(buffer);
+        if (remaining < to_recv) to_recv = remaining;
+
+        buflen = recv(sock, buffer, to_recv, 0);
         if (buflen <= 0) {
             fclose(fp);
             return -1;
-        };
-        fprintf(fp, "%s", buffer);
-        bzero(buffer, sizeof(buffer));
-    }
-    fclose(fp);
+        }
 
+        fwrite(buffer, 1, buflen, fp);
+        total_received += buflen;
+    }
+
+    fclose(fp);
     return 0;
 }
 
@@ -142,6 +183,46 @@ int write_file(char *filename, char *content, size_t size) {
     fclose(fp);
 }
 
+int send_file(int sock, const char *filename) {
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) return -1;
+
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    rewind(fp);
+
+    char size_str[32];
+    snprintf(size_str, sizeof(size_str), "%ld\n", file_size);
+
+    if (send(sock, size_str, strlen(size_str), 0) < 0) {
+        fclose(fp);
+        return -1;
+    }
+    sleep(0.1);
+
+    char *buffer = malloc(file_size);
+    if (!buffer) {
+        fclose(fp);
+        return -1;
+    }
+
+    size_t read_size = fread(buffer, 1, file_size, fp);
+    fclose(fp);
+
+    if (read_size != file_size) {
+        free(buffer);
+        return -1;
+    }
+
+    if (send(sock, buffer, file_size, 0) < 0) {
+        free(buffer);
+        return -1;
+    }
+
+    free(buffer);
+    return 0;
+}
+
 void revstr(char *str) {
     if (!str) return;
     int i = 0;
@@ -156,7 +237,7 @@ void revstr(char *str) {
 }
 
 // https://nachtimwald.com/2017/09/24/hex-encode-and-decode-in-c/#hex-to-binary
-int hexchr2bin(const char hex, char *out) {
+int hexchr2bin(char hex, char *out) {
     if (out == NULL)
         return 0;
 
@@ -173,7 +254,7 @@ int hexchr2bin(const char hex, char *out) {
     return 1;
 }
 
-size_t hexs2bin(const char *hex, unsigned char **out) {
+size_t hexs2bin(char *hex, unsigned char **out) {
     size_t len;
     char b1, b2;
     size_t i;
@@ -205,12 +286,11 @@ size_t hexs2bin(const char *hex, unsigned char **out) {
 }
 
 int decoder_file(int sock) {
-    printf("called decoder_file\n");
     char *folder = "database";
     struct stat st;
     if (stat(folder, &st) == -1) {
-        if (mkdir(folder, 0700) == -1) return 1;
-    }
+        if (mkdir(folder, 0700) == -1) return -1;
+    };
 
     time_t timestamp = time(NULL);
     char strtime[32];
@@ -219,10 +299,13 @@ int decoder_file(int sock) {
     snprintf(strtime, sizeof(strtime), "%ld", timestamp);
     snprintf(filename, sizeof(filename), "%s/%s.jpeg", folder, strtime);
 
-    if (recvto_file(sock, filename) < 0) {
-        perror("recv failed");
-        return -1;
-    };
+    char size_buffer[32];
+    ssize_t size_len = recv(sock, size_buffer, sizeof(size_buffer) - 1, 0);
+    if (size_len <= 0) return -1;
+    size_buffer[size_len] = '\0';
+    size_t expected_size = strtoul(size_buffer, NULL, 10);
+
+    if (recvto_file(sock, filename, expected_size) < 0) return -1;
 
     long file_size;
     char *file_content = read_file(filename, &file_size);
@@ -231,15 +314,14 @@ int decoder_file(int sock) {
     revstr(file_content);
 
     unsigned char *decoded;
-
     size_t decoded_size = hexs2bin(file_content, &decoded);
-    if (decoded_size < 0) {
+    if (decoded_size == 0) {
         free(file_content);
         free(decoded);
         return -1;
     }
 
-    write_file(filename, decoded, decoded_size);
+    write_file(filename, (char *)decoded, decoded_size);
 
     free(file_content);
     free(decoded);
@@ -247,7 +329,18 @@ int decoder_file(int sock) {
 }
 
 int downloader_file(int sock) {
-    char *message = "File download initiated";
-    send(sock, message, strlen(message), 0);
+    char filename[128];
+    char target[256];
+    char *folder = "database";
+
+    ssize_t buflen = recv(sock, filename, sizeof(filename) - 1, 0);
+    if (buflen <= 0) return -1;
+    filename[buflen] = '\0';
+    if (filename[buflen - 1] == '\n') filename[buflen - 1] = '\0';
+
+    snprintf(target, sizeof(target), "%s/%s", folder, filename);
+    struct stat st;
+    if (stat(target, &st) == -1) return -1;
+    send_file(sock, target);
     return 0;
 }
