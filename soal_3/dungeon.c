61ce73c2a34fcb2a4ff422bc898b1db297cdfa7c
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-05-01 14:25:09 +0700

    initial code server for soal 3

--- soal_3/dungeon.c
+++ soal_3/dungeon.c
@@ -1,6 +1,171 @@
+#define _XOPEN_SOURCE 700
+
+#include <arpa/inet.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "shop.c"
+
+#define PORT 1337
+
+int make_socket();
+void reap_zombies(int sig);
+void handler(int sock);
+
+int get_stats(int sock, struct Player *player);
+int get_inventory(int sock, struct Player *player);
+int change_weapon(int sock, struct Player *player);
 
 int main(int argc, char *argv[]) {
+    struct sigaction sa;
+    sa.sa_handler = reap_zombies;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGCHLD, &sa, NULL);
+
+    int sockfd = make_socket();
+    if (sockfd < 0) {
+        perror("socket setup failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    while (1) {
+        int client_sock = accept(sockfd, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_sock < 0) continue;
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            close(client_sock);
+            continue;
+        } else if (pid == 0) {
+            close(sockfd);
+            handler(client_sock);
+            close(client_sock);
+            exit(0);
+        } else {
+            close(client_sock);
+        }
+    }
+
+    close(sockfd);
+
     return 0;
+}
+
+void reap_zombies(int sig) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+int make_socket() {
+    struct sockaddr_in address;
+    int sockfd;
+    socklen_t addrlen = sizeof(address);
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) return -1;
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(PORT);
+
+    int opt = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    if ((bind(sockfd, (struct sockaddr *)&address, addrlen)) < 0) return -1;
+    if (listen(sockfd, 5) < 0) return -1;
+
+    return sockfd;
+}
+
+void handler(int sock) {
+    struct Weapon defaultWeapon = {"Fists", 0, 5, 0, 0, NONE};
+
+    struct Inventory inventory = {NULL, 5, 0};
+    inventory.weapons = malloc(sizeof(struct Weapon) * inventory.capacity);
+    if (inventory.weapons == NULL) return;
+    inventory.weapons[inventory.size++] = defaultWeapon;
+
+    struct Player player = {500, 0, inventory, 0};
+
+    char buffer[1024];
+    int buflen;
+    while (1) {
+        buflen = recv(sock, buffer, sizeof(buffer), 0);
+        if (buflen <= 0) break;
+        buffer[buflen - 1] = '\0';
+
+        if (strcmp(buffer, "exit") == 0) break;
+        if (strcmp(buffer, "stats") == 0) {
+            if (get_stats(sock, &player) < 0) break;
+        }
+        if (strcmp(buffer, "inventory") == 0) {
+            if (get_inventory(sock, &player) < 0) break;
+        }
+        if (strcmp(buffer, "change") == 0) {
+            if (change_weapon(sock, &player) < 0) break;
+        }
+        if (strcmp(buffer, "weapons") == 0) {
+            if (available_weapons(sock) < 0) break;
+        }
+        if (strcmp(buffer, "buy") == 0) {
+            if (buy_weapon(sock, &player) < 0) break;
+        }
+    }
+
+    free(inventory.weapons);
+}
+
+int get_stats(int sock, struct Player *player) {
+    char buffer[1024];
+    snprintf(buffer, sizeof(buffer),
+             "GOLD=%d;KILLS=%d;WEAPON=%s;DMG=%d;PASSIVE=%s\n",
+             player->gold, player->kills,
+             player->inventory.weapons[player->equippedWeapon].name,
+             player->inventory.weapons[player->equippedWeapon].damage,
+             WeaponPassiveStr[player->inventory.weapons[player->equippedWeapon].passiveType]);
+
+    int len = strlen(buffer);
+    if (send(sock, buffer, len, 0) != len) return -1;
+}
+
+int get_inventory(int sock, struct Player *player) {
+    char buffer[1024];
+    int len = 0;
+    for (int i = 0; i < player->inventory.size; i++) {
+        struct Weapon *weapon = &player->inventory.weapons[i];
+        len += snprintf(buffer + len, sizeof(buffer) - len,
+                        "NAME=%s:PASSIVE=%s;",
+                        weapon->name,
+                        WeaponPassiveStr[weapon->passiveType]);
+    }
+    len += snprintf(buffer + len, sizeof(buffer) - len,
+                    "EQUIPPED=%d\n\n", player->equippedWeapon);
+    buffer[len - 1] = '\0';
+
+    if (send(sock, buffer, len, 0) != len) return -1;
+    return 0;
+}
+
+int change_weapon(int sock, struct Player *player) {
+    char buffer[128];
+    int len = recv(sock, buffer, sizeof(buffer), 0);
+    if (len <= 0) return -1;
+    buffer[len - 1] = '\0';
+    int index = atoi(buffer);
+
+    if (index < 0 || index >= player->inventory.size) {
+        snprintf(buffer, sizeof(buffer), "Invalid weapon index\n");
+        send(sock, buffer, strlen(buffer), 0);
+        return -1;
+    }
+
+    player->equippedWeapon = index;
 }
--- soal_3/player.c
+++ soal_3/player.c
@@ -1,6 +1,53 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+
+#define HOST "127.0.0.1"
+#define PORT 1337
+
+int connect_socket();
+void handler(int sockfd, int opt);
 
 int main(int argc, char *argv[]) {
+    // int sockfd = connect_socket();
+
+    // if (sockfd < 0) {
+    //     perror("connection failed");
+    //     exit(EXIT_FAILURE);
+    // }
+    // printf("Connected to address %s:%d\n", HOST, PORT);
+
+    int opt;
+
+    while (1) {
+        printf("1. Send input file to server\n");
+        printf("2. Download file from server\n");
+        printf("3. Exit\n");
+        printf(">>> ");
+        scanf("%d", &opt);
+        // handler(sockfd, opt);
+    }
+
+    // close(sockfd);
     return 0;
 }
+
+int connect_socket() {
+    int sockfd;
+    struct sockaddr_in address;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) return -1;
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    address.sin_addr.s_addr = inet_addr(HOST);
+
+    if (connect(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) return -1;
+
+    return sockfd;
+}
+
+void handler(int sockfd, int opt) {}
