8501c4500065728fccce901f7ac4fcd5c2235568
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-05-02 20:59:49 +0700

    update log and error handler soal 1

--- soal_1/image_client.c
+++ soal_1/image_client.c
@@ -1,15 +1,18 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define HOST "127.0.0.1"
 #define PORT 1337
 
+void cleanline();
 int connect_socket();
-void handler(int sockfd, int opt);
+int handler(int sockfd, int opt);
 int send_file(int sockfd, const char *filename);
 int recvto_file(int sock, char *filename, size_t size);
 
@@ -17,10 +20,10 @@ int main(int argc, char *argv[]) {
     int sockfd = connect_socket();
 
     if (sockfd < 0) {
-        perror("connection failed");
+        printf("Gagal connect ke server\n\n");
         exit(EXIT_FAILURE);
     }
-    printf("Connected to address %s:%d\n", HOST, PORT);
+    printf("Connected to address %s:%d\n\n", HOST, PORT);
 
     int opt;
 
@@ -30,11 +33,16 @@ int main(int argc, char *argv[]) {
         printf("3. Exit\n");
         printf(">>> ");
         scanf("%d", &opt);
+        cleanline();
         handler(sockfd, opt);
     }
 
     close(sockfd);
     return 0;
+}
+
+void cleanline() {
+    while (getchar() != '\n');
 }
 
 int connect_socket() {
@@ -52,7 +60,7 @@ int connect_socket() {
     return sockfd;
 }
 
-void handler(int sockfd, int opt) {
+int handler(int sockfd, int opt) {
     char *folder = "secrets";
     char filename[256];
 
@@ -63,33 +71,56 @@ void handler(int sockfd, int opt) {
             printf("Enter the file name: ");
             scanf("%s", filename);
             snprintf(target, sizeof(target), "%s/%s", folder, filename);
-            send(sockfd, "decode\n", strlen("decode\n"), 0);
-            send_file(sockfd, target);
+            if (send(sockfd, "decode\n", strlen("decode\n"), 0) < 0) return -1;
+            if (send_file(sockfd, target) < 0) return -1;
+
+            char res[256];
+            ssize_t res_len = recv(sockfd, res, sizeof(res), 0);
+            if (res_len <= 0) break;
+            res[res_len] = '\0';
+            printf("Server: %s\n\n", res);
             break;
         case 2:
             printf("Enter the file name to download: ");
             scanf("%s", filename);
-            send(sockfd, "download\n", strlen("download\n"), 0);
+            if (send(sockfd, "download\n", strlen("download\n"), 0) < 0) return -1;
             snprintf(target, sizeof(target), "%s\n", filename);
             sleep(0.1);
-            send(sockfd, target, strlen(target), 0);
-            char size_buffer[32];
+            if (send(sockfd, target, strlen(target), 0) < 0) return -1;
+
+            char size_buffer[128];
             ssize_t size_len = recv(sockfd, size_buffer, sizeof(size_buffer) - 1, 0);
             if (size_len <= 0) break;
             size_buffer[size_len] = '\0';
+
+            if (strlen(size_len) > 10) {
+                printf("Server: %s\n\n", size_buffer);
+                break;
+            }
+
             size_t expected_size = strtoul(size_buffer, NULL, 10);
-            recvto_file(sockfd, filename, expected_size);
+
+            if (recvto_file(sockfd, filename, expected_size) < 0) return -1;
             break;
         case 3:
+            if (send(sockfd, "exit\n", strlen("exit\n"), 0) < 0) return -1;
             close(sockfd);
             exit(EXIT_SUCCESS);
-            return;
+            return 0;
         default:
-            printf("Invalid option\n");
+            printf("Invalid option, try again\n\n");
     }
+
+    return 0;
 }
 
 int send_file(int sockfd, const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) < 0) {
+        printf("Salah nama text file input\n\n");
+        return -1;
+    }
+
     FILE *fp = fopen(filename, "rb");
     if (!fp) return -1;
 
@@ -101,6 +132,7 @@ int send_file(int sockfd, const char *filename) {
     snprintf(size_str, sizeof(size_str), "%ld\n", file_size);
 
     if (send(sockfd, size_str, strlen(size_str), 0) < 0) {
+        fflush(fp);
         fclose(fp);
         return -1;
     }
@@ -108,11 +140,13 @@ int send_file(int sockfd, const char *filename) {
 
     char *buffer = malloc(file_size);
     if (!buffer) {
+        fflush(fp);
         fclose(fp);
         return -1;
     }
 
     size_t read_size = fread(buffer, 1, file_size, fp);
+    fflush(fp);
     fclose(fp);
 
     if (read_size != file_size) {
@@ -130,7 +164,6 @@ int send_file(int sockfd, const char *filename) {
 }
 
 int recvto_file(int sock, char *filename, size_t size) {
-    ssize_t buflen;
     FILE *fp;
     char buffer[1024];
     size_t total_received = 0;
@@ -143,8 +176,14 @@ int recvto_file(int sock, char *filename, size_t size) {
         size_t to_recv = sizeof(buffer);
         if (remaining < to_recv) to_recv = remaining;
 
-        buflen = recv(sock, buffer, to_recv, 0);
+        ssize_t buflen;
+        do {
+            buflen = recv(sock, buffer, to_recv, 0);
+        } while (buflen < 0 && errno == EINTR);
+
         if (buflen <= 0) {
+            perror("error receiving file");
+            fflush(fp);
             fclose(fp);
             return -1;
         }
@@ -153,6 +192,7 @@ int recvto_file(int sock, char *filename, size_t size) {
         total_received += buflen;
     }
 
+    fflush(fp);
     fclose(fp);
     return 0;
 }
